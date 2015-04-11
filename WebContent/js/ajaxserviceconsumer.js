8d216797ec882e0f8a50206a22312bf8d3005b22
Rustam Kamberov <rustam.kamberov@gmail.com>
2015-04-11 16:48:02 -0600

    t

---
+++ WebContent/js/ajaxserviceconsumer.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+/*
+ * $(document).ready(function() { $.ajax({ url:
+ * "http://localhost:8080/sensor-rest-service/service/sensor/1"
+ * }).then(function(data) { $('.greeting-id').append(data.id);
+ * $('.greeting-temp').append(data.temp);
+ * $('.greeting-heartrate').append(data.heartRate); }); });
+ */
+
+window.setInterval(function() {
+	$(document).ready(function() {
+		$.ajax({
+			url : "http://localhost:8080/sensor-rest-service/service/sensor/"
+		}).then(function(data) {
+			$('.greeting-id').html("sensor id: " + data[2].id);
+			$('.greeting-temp').html("temperature: " + data[2].temp);
+			$('.greeting-heartrate').html("heart rate: " + data[2].heartRate);
+		});
+		var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+		var marker = new google.maps.Marker({
+		    position: map.getCenter(),
+		    map: map,
+		    title: 'Click to zoom'
+		  });
+	
+		google.maps.event.addListener(marker, 'click', function() {
+			map.setZoom(8);
+			map.setCenter(marker.getPosition());
+		});
+	});
+}, 1000);
+/*
+ * $.ajax({ type: "GET", dataType: "jsonp", url:
+ * "http://localhost:8080/sensor-rest-service/service/sensor/1", success:
+ * function(data){ alert(data); $('.greeting-id').html(data.id);
+ * $('.greeting-temp').html(data.temp);
+ * $('.greeting-heartrate').html(data.heartRate); }
+ * 
+ * });
+ */
