d202230d48d9a7c654599e1ff634491d487678d9
Rustam Kamberov <rustam.kamberov@gmail.com>
2015-04-17 21:38:34 -0600

    -fixed issues with requesting a web service

--- WebContent/js/ajaxserviceconsumer.js
+++ WebContent/js/ajaxserviceconsumer.js
@@ -1,13 +1,4 @@
-/**
- * 
- */
-/*
- * $(document).ready(function() { $.ajax({ url:
- * "http://localhost:8080/sensor-rest-service/service/sensor/1"
- * }).then(function(data) { $('.greeting-id').append(data.id);
- * $('.greeting-temp').append(data.temp);
- * $('.greeting-heartrate').append(data.heartRate); }); });
- */
+var lng, lat;
 
 window.setInterval(function() {
 	$(document).ready(function() {
@@ -17,35 +8,46 @@ window.setInterval(function() {
 			$('.id').html("sensor id: " + data[2].id);
 			$('.temp').html("temperature: " + data[2].temp);
 			$('.heartrate').html("heart rate: " + data[2].heartRate);
+			$('.long').html("heart rate: " + data[2].location.x);
+			$('.lat').html("heart rate: " + data[2].location.y);
+			lng = data[2].location.x;
+			lat = data[2].location.y;
+			//var point = new google.maps.LatLng(data[2].location.y, data[2].location.y);
+			//createMarker(map, point, 'temp');
+			
 		});
 		/*
-		var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
+		 * var myLatlng = new google.maps.LatLng(-25.363882, 131.044922); var
+		 * marker = new google.maps.Marker({ position : map.getCenter(), map :
+		 * map, title : 'Click to zoom' });
+		 * 
+		 * google.maps.event.addListener(marker, 'click', function() {
+		 * map.setZoom(8); map.setCenter(marker.getPosition()); });
+		 */
+		var myLatlng = new google.maps.LatLng(51.03692904550923,
+				-114.05250939814002);
 		var marker = new google.maps.Marker({
-			position : map.getCenter(),
+			position : myLatlng,
 			map : map,
-			title : 'Click to zoom'
+			title : 'marker last'
 		});
-		 
-		google.maps.event.addListener(marker, 'click', function() {
-			map.setZoom(8);
-			map.setCenter(marker.getPosition());
-		});
-		*/
-		trackDog();
+		trackDog(lat, lng);
 	});
 }, 1000);
 
-function trackDog() {
-
-	
+function trackDog(y, x) {
 
 	var bounds = circle.getBounds();
 	map.fitBounds(bounds);
 	var sw = bounds.getSouthWest();
+	
 	var ne = bounds.getNorthEast();
-	for (var i = 0; i < 100; i++) {
+	/*
+	for (var i = 0; i < 10; i++) {
 		var ptLat = Math.random() * (ne.lat() - sw.lat()) + sw.lat();
+		console.log(ptLat)
 		var ptLng = Math.random() * (ne.lng() - sw.lng()) + sw.lng();
+		console.log(ptLng)
 		var point = new google.maps.LatLng(ptLat, ptLng);
 		if (google.maps.geometry.spherical.computeDistanceBetween(point, circle
 				.getCenter()) < circle.getRadius()) {
@@ -53,5 +55,16 @@ function trackDog() {
 			// break;
 		}
 	}
+	*/
+	var point = new google.maps.LatLng(y, x);
+	createMarker(map, point);
+}
+
+function createMarker(map, point) {
+	var marker = new google.maps.Marker({
+		position : point,
+		map : map
+	});
 	
+	return marker;
 }
